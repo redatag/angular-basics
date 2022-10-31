046b2c9fca5e3c56e0050b1131e35dadec3307f3
Reda Tag <rtag@ncgr.gov.sa>
2022-11-01 01:10:46 +0300

    fetch dat ausing http and observable

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root' // register the service
   //providers: [EmployeeService], or in app module
 })
 export class EmployeeService {
 
-  //rejister the service at module level to be acces by all component in this module
-  constructor() { }
+  private url: string = '/assets/data/employees.json';
+  //register the service at module level to be acces by all component in this module
+  constructor(private http: HttpClient) { }
 
   getEmloyees() {
-    return [
-      {id:1 , name:"reda", age: 33},
-      {id:2 , name:"ahmed", age: 28},
-      {id:3 , name:"malek", age: 20}
-    ];
+    this.http.get(this.url);
+    // return [
+    //   {id:1 , name:"reda", age: 33},
+    //   {id:2 , name:"ahmed", age: 28},
+    //   {id:3 , name:"malek", age: 20}
+    // ];
   }
 }
